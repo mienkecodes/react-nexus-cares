16c07197677f7033a87a40c0cbc97922fbf29274
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-09-25 11:23:52 +1000

    update api

---
+++ src/api/put-user.js
@@ -0,0 +1,56 @@
+
+async function putUser(id,first_name,last_name,email,username) {
+    const url = `${import.meta.env.VITE_API_URL}/users/${id}/`;
+    const token = window.localStorage.getItem("token");
+    const body = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "username": username,
+    }
+
+    for (let bod in body){
+        if (body[bod] == ""){
+        delete body[bod]
+        }
+    }
+    const response =  await fetch(url, {
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+        const fallbackError = `Error updating user`;
+
+        const data = await response.json().catch(() => {
+        throw new Error(fallbackError);
+        });
+
+        const errorMessages = {
+        usernameError: data?.username,
+        emailError: data?.email,
+        detailError: data?.detail,
+        };
+
+        if (errorMessages) {
+        for (let error in errorMessages) {
+            if (!errorMessages[error]) {
+            delete errorMessages[error];
+            }
+        }
+
+        const errorMessage = Object.values(errorMessages);
+        throw new Error(errorMessage);
+        } else {
+        const errorMessage = fallbackError;
+        throw new Error(errorMessage);
+        }  
+    }
+    return await response.json();
+    }
+
+    export default putUser;
